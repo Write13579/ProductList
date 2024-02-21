603e5538b7233a89a375e418184f4a2fc919a68f
Patryk Baraniak <pb311239@student.polsl.pl>
2024-02-21 18:23:54 +0100

    delete function

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,31 @@ function send() {
   const getColor = document.getElementById("color") as HTMLInputElement;
   const getCalorie = document.getElementById("calorie") as HTMLInputElement;
 
+  if (getName && getColor && getCalorie) {
+    products.push({
+      name: getName.value,
+      color: getColor.value,
+      calories: parseInt(getCalorie.value),
+    });
+  }
+
+  const table = document.getElementById("productlist")!;
+
+  updateTable();
+
+  if (products.length > 0) {
+    if (!table.classList.contains("brama")) {
+      table.classList.add("brama");
+    }
+  }
+}
+
+function deleteFromList(index: number) {
+  products = products.filter((_el, idx) => idx !== index);
+  updateTable();
+}
+
+function updateTable() {
   const HEADER = `<tr id="mains">
   <th>names</th>
   <th>colors</th>
@@ -19,28 +44,16 @@ function send() {
 
   const table = document.getElementById("productlist")!;
 
-  if (getName && getColor && getCalorie) {
-    products.push({
-      name: getName.value,
-      color: getColor.value,
-      calories: parseInt(getCalorie.value),
-    });
-  }
   const addText = products
     .map(
-      (product) => `<tr>
+      (product, index) => `<tr>
 <td>${product.name === "" ? `<i>nothing typed</i>` : product.name}</td>
 <td>${product.color === "" ? `<i>nothing typed</i>` : product.color}</td>
 <td>${isNaN(product.calories) ? `<i>not number</i>` : product.calories}</td>
+<td><button onclick="deleteFromList(${index})">delete</button></td>
 </tr>`
     )
     .join("");
 
   table.innerHTML = HEADER + addText;
-
-  if (products.length > 0) {
-    if (!table.classList.contains("brama")) {
-      table.classList.add("brama");
-    }
-  }
 }
